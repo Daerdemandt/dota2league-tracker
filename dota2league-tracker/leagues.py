d4a7ebb7d2afbf81c677e138004e4b07f97f1264
Roman Kuzmichev <daerdemandt@gmail.com>
2016-09-14 20:10:38 +0300

    Implement 'list' in leagues

--- dota2league-tracker/leagues.py
+++ dota2league-tracker/leagues.py
@@ -6,6 +6,15 @@ league_validator = Schema({
     'is_tracked' : Coerce(bool)
 })
 
+# TODO: use that in coerce
+def read_bool(string):
+    if string.lower() == 'true':
+        return True
+    elif string.lower() == 'false':
+        return False
+    else:
+        raise ValueError('Expected boolean value, got ' + str(string))
+
 class Leagues():
     def __init__(self, db):
         self.db = MongoCRUD(db, 'leagues', league_validator)
@@ -13,6 +22,10 @@ class Leagues():
         raise NotImplementedError
     def untrack(self, steam_id):
         raise NotImplementedError
-    def list(self, tracked = None):
+    def list(self, is_tracked = None):
+        if is_tracked == None:
+            return self.db.list()
+        else:
+            return self.db.list(is_tracked=read_bool(is_tracked))
         raise NotImplementedError
 
