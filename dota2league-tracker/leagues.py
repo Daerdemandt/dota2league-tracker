1512fd2ff17c9aed6b4f8efdb0e5e248e88d3d00
Roman Kuzmichev <daerdemandt@gmail.com>
2016-09-19 17:54:24 +0300

    make track/untrack update statuses in db

--- dota2league-tracker/leagues.py
+++ dota2league-tracker/leagues.py
@@ -18,14 +18,24 @@ def read_bool(string):
 class Leagues():
     def __init__(self, db):
         self.db = MongoCRUD(db, 'leagues', league_validator)
+
+    def _update_tracking_status(self, steam_id, new_status):
+        try:
+            league = self.db.read(steam_id=int(steam_id))
+            self.db.update(league['_id'], {'is_tracked':new_status})
+        except KeyError:
+            league = self.db.create(steam_id=int(steam_id), is_tracked=new_status)
+        return str(league['_id'])
+
     def track(self, steam_id):
-        raise NotImplementedError
+        return self._update_tracking_status(steam_id, True)
+
     def untrack(self, steam_id):
-        raise NotImplementedError
+        return self._update_tracking_status(steam_id, False)
+
     def list(self, is_tracked = None):
         if is_tracked == None:
             return self.db.list()
         else:
             return self.db.list(is_tracked=read_bool(is_tracked))
-        raise NotImplementedError
 
